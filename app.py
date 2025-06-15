2d059aedc625653bdac2286e614d90ab26c153e0
dhnsnas <fahtiningtyas@gmail.com>
2025-06-15 20:44:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,8 @@ st.title("Prediksi SoC Baterai VRLA dengan SVR")
 st.markdown("Aplikasi ini menggunakan model Support Vector Regression (SVR) untuk memprediksi State of Charge (SoC) berdasarkan input parameter baterai.")
 
 # Load model
-model_path = "model_svr.pkl"
-df_path = "df.pkl"
-
-if os.path.exists(model_path) and os.path.exists(df_path):
-    svr_model = joblib.load(model_path)
-    df = pd.read_pickle(df_path)
-else:
-    st.error("❌ File model atau data tidak ditemukan. Pastikan 'model_svr.pkl' dan 'df.pkl' tersedia di folder.")
-    st.stop()
+model_svr =joblib.load ('model_svr.pkl')
+df = joblib.load  ('df.pkl')
 
 # Sidebar input user
 st.sidebar.header("Input Parameter")
